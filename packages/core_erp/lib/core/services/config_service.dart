be513177eb5f80f6e297d89a19684ed781808a18
Rutuparn Puranik <rockkulkarni37@gmail.com>
2026-07-18 02:40:11 +0530

    feat: add new configuration flags for enhancements, challans, catalog, and purchase modules

--- packages/core_erp/lib/core/services/config_service.dart
+++ packages/core_erp/lib/core/services/config_service.dart
@@ -39,6 +39,10 @@ class ConfigService {
       },
       "allowCustomActions": true,
     },
+    "enhancements": {"catalogInventory": true},
+    "challans": {"singleTypeView": true},
+    "catalog": {"purchaseItems": true},
+    "purchase": {"flowV2": true},
     "update": {"channel": "stable", "latest_version": "1.0.0"},
   };
 
